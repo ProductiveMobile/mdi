9983c84c57e4faedd34449a8497484443a05228a
Mikhail Davydov <mdavydov@productivemobile.com>
2016-02-05 10:35:11 +0100

    Generate component names

---
+++ icons/index.js
@@ -0,0 +1,27 @@
+var requireIcons = require.context('./svg', false, /^\.\/.*\.svg$/);
+
+var reFileName = /^\.\/(.*)\.svg/;
+
+/**
+ * @param {String} iconName
+ * @returns {String}
+ */
+function getComponentName(iconName) {
+  return iconName.split('-').map(function (iconNamePart) {
+    return iconNamePart.charAt(0).toUpperCase() + iconNamePart.slice(1);
+  }).join('');
+}
+
+var exports = {
+  icons: {}
+};
+
+requireIcons.keys().forEach(function (icons, iconFilename) {
+  var iconName = reFileName.exec(iconFilename)[1];
+  var iconComponent = requireIcons(iconFilename);
+  var iconComponentName = getComponentName(iconName);
+  exports.icons[iconName] = iconComponent;
+  exports[iconComponentName] = iconComponent;
+});
+
+module.exports = exports;
